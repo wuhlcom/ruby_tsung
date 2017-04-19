13cd2ed101255954651e89eb4b43ac9f9a22f149
wuhlcom <wuhlcom@qq.com>
2017-04-19 11:08:41 +0800

    delete tsungLogs

--- tsharkMqtt/cDRb_tshark.rb
+++ tsharkMqtt/cDRb_tshark.rb
@@ -23,23 +23,28 @@ DRb.start_service
 tshark=DRbObject.new_with_uri(URI_ADDR)
 #0 生成tsung配置文件
 #1 开始抓包
+puts "capture beginning....."
 l_tshark=ZL::Tshark.new(pkgdir,filename) 	
 #本地客户端抓包
 l_tshark.capture(cap_filter,10,1)	
 #远程客户端抓包
 tshark.capture(cap_filter,filesize,filenum)
 #2 开始tsung
+puts "tsung starting....."
 tsung=ZL::Tsung.new(xmlpath)
 flag=tsung.tsung_start
 #3 结束tsung，停止抓包
+puts "tsung end and tshark stop....."
 if flag==true
   tshark.linuxpkill(tshark_process)
   l_tshark.linuxpkill(tshark_process)
 end
 #4 解析报文并写入数据库
+puts "parser packets..."
 l_tshark.write_records(ex_filter,pub_filter,rev_filter)
 tshark.write_records(ex_filter,pub_filter,rev_filter)
 #5 写入最终结果 
+puts "result..."
 l_tshark.write_result
 #   Benchmark.bm(7) do |x|
 #  	 x.report("pubs"){ 		
