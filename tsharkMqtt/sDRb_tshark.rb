93fa15017b16503f13aebd1a4170212f5e40cb71
wuhlcom <wuhlcom@qq.com>
2017-04-19 11:11:02 +0800

    sDRb_tshark.rb

--- tsharkMqtt/sDRb_tshark.rb
+++ tsharkMqtt/sDRb_tshark.rb
@@ -6,11 +6,10 @@ URI_ADDR="druby://#{ip}:#{port}"
 class SDRbTshark<ZL::Tshark
 end
 
-pkgdir="testpkgdir"
+pkgdir="testcap"
 filename="test"
 puts "DRB undumped SERVER #{URI_ADDR} is running..."
-FRONT_OBJECT=SDRbTshark.new(pkgdir,filename)
+FRONT_OBJECT=SDRbTshark.new()
 p FRONT_OBJECT
 DRb.start_service(URI_ADDR, FRONT_OBJECT)
 DRb.thread.join
-~                     
