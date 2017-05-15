39b3efb67e95786bdf14264918399ac48d159f85
ubuntu14042 <wuhlcom@qq.com>
2017-05-15 17:34:03 +0800

    xml

--- tsharkMqtt/config.rb
+++ tsharkMqtt/config.rb
@@ -5,7 +5,7 @@
 ###tshark 服务名
 @tshark_process="tshark"
 ###xmlpath tsung文件路径
-@xmlpath="./xmls/mqtt_csv.xml"
+@xmlpath="./xmls/mqtt.xml"
 @sub_csv="./csvs/sub.csv"
 @pub_csv="./csvs/pub.csv"
 ###############ruby server ip and port
@@ -14,11 +14,11 @@
 @remote_clientIP=@drbsrv_ip #tsung remote client
 @mqttsrv_ip="192.168.10.188" #mqtt server ip
 
-@acc_num=10000 #创建redis用户数量,即tsung中一个phase中users数量
+@acc_num=1000 #创建redis用户数量,即tsung中一个phase中users数量
 @maxusers=40000 #一台客户机总users数量
 @sub_arrivalrate='50'#单位时间内产生user数量
-@sub_duration=250 #一个phase中生成users总时长
-@pub_duration=250 #一个phase中生成users总时长
+@sub_duration=50 #一个phase中生成users总时长
+@pub_duration=50 #一个phase中生成users总时长
 @sub_timeout=@sub_duration+@pub_duration+30    #每个user等待收到pub消息时长,要大于sub和pub的和
 
 @redis_ip="192.168.10.8" #redis server ip
